3b22211101a9f2850b7c55feac9fef9e6c092063
Dan Lafeir <danlafeir@users.noreply.github.com>
2025-03-14 23:03:06 -0500

    Add python bootstrapping

---
+++ language-specific/python/bootstrap.sh
@@ -0,0 +1,4 @@
+echo '# python config' >> ~/.zshrc
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
+echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
+echo 'eval "$(pyenv init -)"' >> ~/.zshrc
